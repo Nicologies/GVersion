142c77c14b253d02aab5aa8c7cdc118392b09669
EbenZhang <zhangyingneng@gmail.com>
2017-10-09 12:59:13 +0800

    Able to match 4 digits version in branch name

--- GVersion/VersionStrategies/BranchNameVersionStrategy.cs
+++ GVersion/VersionStrategies/BranchNameVersionStrategy.cs
@@ -10,10 +10,15 @@ namespace GVersion.VersionStrategies
         public Version GetVersion(IRepository repo, Version knownHighestVersion)
         {
             var branchName = repo.Head.FriendlyName;
-            var match = Regex.Match(branchName, @".*(\d+\.\d+\.\d+).*");
+            var match = Regex.Match(branchName, @".*?((\d+\.){2,3}\d+).*");
             if (!match.Success) return new Version(0, 0, 0, 0);
             var versionStr = match.Groups[1];
-            return new Version(versionStr + ".0");
+            var ret = new Version(versionStr.Value);
+            if (ret.Revision == -1)
+            {
+                ret = new Version(ret.Major, ret.Minor, ret.Build, 0);
+            }
+            return ret;
         }
 
         public string Name => nameof(BranchNameVersionStrategy);
