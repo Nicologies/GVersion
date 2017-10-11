179c02aced5f48dd0b925576ab2a5af73b010beb
Nicole <nicoleflopy@gmail.com>
2017-10-11 19:30:58 +0800

    Dont publish to Choco
    have no time to investigate how to provide the choco verification approach

--- PublishToChoco.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param (   
-    [Parameter(Mandatory=$True)]
-    [string]$apikey
-)
-$ErrorActionPreference = 'Stop'
-cd dist
-choco push --source "https://chocolatey.org" -k="'$apikey'"
-cd ..
